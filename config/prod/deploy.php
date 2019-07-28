6d043a04f7ca5e7c701b89d863ed091b34cc9d14
Vitaly Nokhrin <vin@crtweb.ru>
2019-07-28 15:13:04 +0500

    fix deploy settiongs creating symlink to uploads

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -55,7 +55,7 @@ return new class extends DefaultDeployer
         }
 
         $this->runRemote('ln -s ../../shared/.env ./.env ');
-        $this->runRemote('ln -s ../../shared/upload ./public/uploads ');
+        $this->runRemote('ln -s ../../shared/uploads ./public/uploads ');
     }
 
     // run some local or remote commands after the deployment is finished
